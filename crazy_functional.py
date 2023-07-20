f8dd6f8e64455b1730bb480d8a95f767b2cf712c
QQisQQ <1399858762@qq.com>
2023-07-20 12:40:55 +0800

    自用插件

--- crazy_functional.py
+++ crazy_functional.py
@@ -212,13 +212,13 @@ def get_crazy_functions():
     ###################### 第三组插件 ###########################
     # [第三组插件]: 尚未充分测试的函数插件
     try:
-        from crazy_functions.NewBing语法纠错 import NewBing语法纠错
+        from crazy_functions.NewBing工具箱 import NewBing语法纠错
         function_plugins.update({
-            "NewBing语法纠错": {
+            "NewBing工具箱": {
                 "Color": "stop",
                 "AsButton": False,  # 加入下拉菜单中
                 "AdvancedArgs": True, # 调用时，唤起高级参数输入区（默认False）
-                "ArgsReminder": "随便输入点什么东西在这表示双引号模式", # 高级参数输入区的显示提示
+                "ArgsReminder": "模型换成newbing且注意一定要是creative模式", # 高级参数输入区的显示提示
                 "Function": HotReload(NewBing语法纠错)
             }
         })
--- crazy_functions/NewBing工具箱.py
+++ crazy_functions/NewBing工具箱.py
@@ -2,7 +2,7 @@ from toolbox import CatchException, update_ui
 from .crazy_utils import request_gpt_model_in_new_thread_with_ui_alive
 
 @CatchException
-def NewBing语法纠错(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
+def NewBing工具箱(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
     """
     txt             输入栏用户输入的文本，例如需要翻译的一段话，再例如一个包含了待处理文件的路径
     llm_kwargs      gpt模型参数，如温度和top_p等，一般原样传递下去就行
@@ -13,12 +13,12 @@ def NewBing语法纠错(txt, llm_kwargs, plugin_kwargs, chatbot, history, system
     web_port        当前软件运行的端口号
     """
     history = []    # 清空历史，以免输入溢出
-    chatbot.append(("这是什么功能？", "[Local Message] New Bing禁止联网并且仅纠错随后的段落"))
+    chatbot.append(("这是什么功能？", "[Local Message] 使得New Bing禁止联网并且仅纠错随后的段落"))
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面 # 由于请求gpt需要一段时间，我们先及时地做一次界面更新
     if ("advanced_arg" in plugin_kwargs) and (plugin_kwargs["advanced_arg"] == ""): plugin_kwargs.pop("advanced_arg")
-    advanced_arg = plugin_kwargs.get("advanced_arg", "")
+    advanced_arg = plugin_kwargs.get("advanced_arg", "correct")
 
-    if advanced_arg == "":
+    if advanced_arg == "correct":
         i_say = r"Can you help me ensure that the grammar and the spelling is correct? " +\
                 r"Do not try to polish the text, if no mistake is found, tell me that this paragraph is good." +\
                 r"If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, " +\
@@ -31,12 +31,14 @@ def NewBing语法纠错(txt, llm_kwargs, plugin_kwargs, chatbot, history, system
                 r"| How **is** you? | How **are** you? |""\n" +\
                 r"| Do you **knows** what **is** **it**? | Do you **know** what **it** **is** ? |""\n" +\
                 r"The following paragraphs are taken from an academic paper. " +\
-                r"You need to report all grammar and spelling mistakes following the example above and without searching the internet."+\
-                + "\n\n" +\
+                r"You need to report all grammar and spelling mistakes following the example above and without searching the internet.""\n\n" +\
                 "\"" + txt + "\"" 
     else:
-        i_say = "\"" + advanced_arg + "\""
-        
+        i_say = r"The following paragraphs are taken from an academic paper. Polish the writing to meet the academic style, " +\
+                r"improve the spelling, grammar, clarity, concision and overall readability. When necessary, rewrite the whole sentence. Ensure without searching the internet. " +\
+                r"Furthermore, list all modification and explain the reasons to do so in markdown table." + "\n\n" +\
+                "\"" + txt + "\""
+
     gpt_say = yield from request_gpt_model_in_new_thread_with_ui_alive(
         inputs=i_say, inputs_show_user=i_say, 
         llm_kwargs=llm_kwargs, chatbot=chatbot, history=[], 
